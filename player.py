6f86b70a5545d9dba2c9ce071a6cf670787c6a64
Gradient Descent <48599511+SGD2718@users.noreply.github.com>
2023-06-15 21:09:49 -0700

    Initial commit

--- player.py
+++ player.py
@@ -10,11 +10,15 @@ class Player:
         :param name: player's name
         :param hand: the player's cards
         """
-        self.name = name
-        self.hand = [] if hand is None else hand
+        self.name: str = name
+        self.hand: list[Card] = [] if hand is None else hand
+        self.canJumpIn: bool = False
 
     def __str__(self):
         return self.name
+
+    def __int__(self):
+        return len(self.hand)
 
     def draw(self, cards: list[Card]) -> None:
         """
@@ -23,4 +27,22 @@ class Player:
         """
         self.hand += cards
 
-    def play(self):
+    def play(self, cards: Card | list[Card]) -> None:
+        """
+        Plays cards from the player's and
+        :param cards: the cards being played
+        """
+        if isinstance(cards, list):
+            for card in cards:
+                assert (card in self.hand)
+                self.hand.remove(card)
+        else:
+            assert (cards in self.hand)
+            self.hand.remove(cards)
+
+
+class Hand:
+    """UNO Player Hand class"""
+
+    def __init__(self, cards, rules):
+        pass
